c0bd9bfbf6f63c20f57d3186b416d4fe04c21f88
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-06-27 16:06:20 +0300

    Add files via upload

---
+++ Java Multithreading 2224 Snake 12/src/com/company/Room.java
@@ -0,0 +1,78 @@
+package com.company;
+
+public class Room {
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+    public static Room game;
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+
+    public static void main(String[] args) {
+        Snake snake = new Snake(0,0);
+        game = new Room(400,400, snake);
+        snake.setDirection(SnakeDirection.DOWN);
+        game.createMouse();
+        game.run();
+    }
+
+    public void createMouse() {
+        int x = (int)(Math.random()*width);
+        int y = (int)(Math.random()*height);
+        mouse = new Mouse(x,y);
+    }
+
+    public void eatMouse() {
+        createMouse();
+    }
+
+    public void sleep() {
+
+    }
+
+    public void run(){
+        sleep();
+    }
+
+    public void print(){
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+}
